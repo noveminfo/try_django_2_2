7153e5d1646514670b531a7045543cdaa54d0b9a
Kate <kate@kate.com>
2019-09-01 00:50:14 +0900

    view and templates

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
-	return HttpResponse("<h1>Hello World</h1>")
+	my_title = "Hello there...."
+	return render(request, "hello_world.html", {"title": my_title})
 
 
 def about_page(request):
-	return HttpResponse("<h1>About Us</h1>")
+	# my_title = "Hello there...."
+	return render(request, "about.html", {"title": "About Us"})
 
 def contact_page(request):
-	return HttpResponse("<h1>Contact Us</h1>")
+	return render(request, "hello_world.html", {"title": "Contact Us"})
+
+def example_page(request):
+	context = {"title": "Example"}
+	template_name = "hello_world.html"
+	template_obj = get_template(template_name)
+	rendered_string = template_obj.render(context)
+	print(rendered_string)
+	return HttpResponse(rendered_string)
+	
